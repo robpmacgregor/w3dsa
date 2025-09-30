423fe2b9297e2d2e818d37d64664bf4653c29874
robpmacgregor <robpmacgregor@gmail.com>
2025-09-30 15:26:41 +0100

    implements fibWithForLoop

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,3 +1,17 @@
-export function fib(): number[] {
-    return [];
+export function fibWithForLoop(): number[] {
+    const seq: number[] = [];
+    let prev2: number = 0;
+    let prev1: number = 1;
+    let newFibo: number;
+
+    seq.push(prev2);
+    seq.push(prev1);
+    for (let i=0; i <=18; i++) {
+        newFibo = prev1 + prev2;
+        seq.push(newFibo);
+        prev2 = prev1;
+        prev1 = newFibo;
+    }
+    
+    return seq
 }
