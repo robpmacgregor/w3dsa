2b9b2c6c95eabf8bdc2c38f0b22579b264f8c6a8
robpmacgregor <robpmacgregor@gmail.com>
2025-10-07 10:39:34 +0100

    implements traversal functions for array backed bintree

--- src/arrayBackedBinaryTree.test.ts
+++ src/arrayBackedBinaryTree.test.ts
@@ -1,21 +1,11 @@
 import { leftChildIndexOf, rightChildIndexOf, ArrayBackedBinaryTree } from './arrayBackedBinaryTree'
+import { preOrderTraversal, inOrderTraversal, postOrderTraversal } from './arrayBackedBinaryTree';
 
-describe("Array backed binary tree woorks correctly", () => {    
-    test("leftChildIndex returns the correct index value", () => {
-        const index = 2;
-        const expected = 5;
-
-        expect(leftChildIndexOf(index)).toBe(expected);
-    });
-    test("rightChildIndex returns the correct index value", () => {
-        const index = 2;
-        const expected = 6;
-
-        expect(rightChildIndexOf(index)).toBe(expected);
-    });
-    test("ArrayBackedBinaryTree set method stores data at the correct index", () => {
-        const tree: ArrayBackedBinaryTree<string> = new ArrayBackedBinaryTree();
-
+describe("Array backed binary tree woorks correctly", () => {   
+    let tree: ArrayBackedBinaryTree<string>;
+    
+    beforeEach(() => {
+        tree = new ArrayBackedBinaryTree()
         tree.setNode(0,"R");
         tree.setNode(leftChildIndexOf(0), "A");
         tree.setNode(rightChildIndexOf(0), "B");
@@ -56,6 +46,23 @@ describe("Array backed binary tree woorks correctly", () => {
             ),
             "G"
         );       
+    });
+
+    test("leftChildIndex returns the correct index value", () => {
+        const index = 2;
+        const expected = 5;
+
+        expect(leftChildIndexOf(index)).toBe(expected);
+    });
+
+    test("rightChildIndex returns the correct index value", () => {
+        const index = 2;
+        const expected = 6;
+
+        expect(rightChildIndexOf(index)).toBe(expected);
+    });
+
+    test("ArrayBackedBinaryTree set method stores data at the correct index", () => {
         expect(tree.getNode(0)).toBe("R");
         expect(tree.getNode(1)).toBe("A");
         expect(tree.getNode(2)).toBe("B");
@@ -72,4 +79,34 @@ describe("Array backed binary tree woorks correctly", () => {
         expect(tree.getNode(13)).toBe(undefined);
         expect(tree.getNode(14)).toBe("G");
     })
+
+    test("Pre-order traversal coreectly traverses the tree", () => {
+        const arr: (string | undefined)[] = []; 
+        const expected = ['R', 'A', 'C', undefined, undefined, 'D', undefined, undefined, 'B', 'E', undefined, undefined, 'F', undefined, 'G', undefined, undefined];
+
+        preOrderTraversal<string>(tree, 0, (el : string|undefined) => arr.push(el))
+
+        expect(arr).toStrictEqual(expected);
+
+    });
+
+    test("In-order traversal coreectly traverses the tree", () => {
+        const arr: (string | undefined)[] = []; 
+        const expected = [undefined, "C", undefined, "A", undefined, "D", undefined, "R" , undefined, "E", undefined, "B", undefined,  "F", undefined, "G", undefined];
+
+        inOrderTraversal<string>(tree, 0, (el : string|undefined) => arr.push(el))
+
+        expect(arr).toStrictEqual(expected);
+
+    });
+
+    test("Post-order traversal coreectly traverses the tree", () => {
+        const arr: (string | undefined)[] = []; 
+        const expected = [undefined, undefined, "C", undefined, undefined, "D", "A", undefined, undefined, "E", undefined, undefined, undefined, "G", "F", "B", "R"];
+
+        postOrderTraversal<string>(tree, 0, (el : string|undefined) => arr.push(el))
+
+        expect(arr).toStrictEqual(expected);
+
+    });
 });
--- src/arrayBackedBinaryTree.ts
+++ src/arrayBackedBinaryTree.ts
@@ -21,4 +21,42 @@ export class ArrayBackedBinaryTree<T> {
     getNode(i: number): T | undefined {
         return this.#tree[i];
     }
+
+    length(): number {
+        return this.#tree.length;
+    }
 } 
+
+export function preOrderTraversal<T>(tree: ArrayBackedBinaryTree<T>, i: number, fn: (value: T|undefined ) => void) {
+    if (i >= tree.length() || typeof(tree.getNode(i)) === "undefined") {
+        fn(undefined);
+        return;
+    }
+
+    fn(tree.getNode(i));
+    preOrderTraversal<T>(tree, leftChildIndexOf(i), fn);
+    preOrderTraversal<T>(tree, rightChildIndexOf(i), fn);
+
+}
+
+export function inOrderTraversal<T>(tree: ArrayBackedBinaryTree<T>, i: number, fn: (value: T|undefined ) => void) {
+    if (i >= tree.length() || typeof(tree.getNode(i)) === "undefined") {
+        fn(undefined);
+        return;
+    }
+
+    inOrderTraversal<T>(tree, leftChildIndexOf(i), fn);
+    fn(tree.getNode(i));
+    inOrderTraversal<T>(tree, rightChildIndexOf(i), fn);
+}
+
+export function postOrderTraversal<T>(tree: ArrayBackedBinaryTree<T>, i: number, fn: (value: T|undefined ) => void) {
+    if (i >= tree.length() || typeof(tree.getNode(i)) === "undefined") {
+        fn(undefined);
+        return;
+    }
+
+    postOrderTraversal<T>(tree, leftChildIndexOf(i), fn);
+    postOrderTraversal<T>(tree, rightChildIndexOf(i), fn);
+    fn(tree.getNode(i));
+}
